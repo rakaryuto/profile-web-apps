069029911f9f5309227feaa0a3b456b069b52427
Ramadhan Agung Karyuto <55395218+rakaryuto@users.noreply.github.com>
2023-05-11 21:34:12 +0700

    Delete Assignment 1 directory

--- Assignment 1/app.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger input');
-const nav = document.querySelector('nav ul');
-
-hamburger.addEventListener('click', function() {
-    nav.classList.toggle('opened');
-})
